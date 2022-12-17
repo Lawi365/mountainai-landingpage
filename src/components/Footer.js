0d04daf8b25c8e4219c8d7a9307cdf7e332e83f9
lawi365 <lawi365@pm.me>
2022-12-16 21:08:07 -0800

    landing page done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,36 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div className="flex justify-between items-center p-2 ml-16 mr-32 text-gray-300 mb-2">
-      <div className="orginal developer font-semibold">
-        <a href="https://cdn.dribbble.com/users/98151/">
-         Dribble
-        </a>
-      </div>
-      <div className="orginal developer font-semibold">
-        <a href="#">
-         Careers
-        </a>
-      </div>
-      <div className="orginal developer font-semibold">
-        <a href="#">
-         Contact
-        </a>
-      </div>
-      <div className="orginal developer font-semibold">
-        <a href="#">
-         Pricing
-        </a>
-      </div>
-      <div className="orginal developer font-semibold hover:text-orange-300">
-        <a href="https://github.com/Lawi365">
-         Lawrence
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex justify-between items-center p-2 ml-16 mr-32 text-gray-300 mb-2">
+            <div className="orginal developer font-semibold">
+                <a href="https://dribbble.com/shots/20145514-House-and-Trees-Illustration-Generator?utm_source=Clipboard_Shot&utm_campaign=patrickjohnson&utm_content=House%20and%20Trees%20Illustration%20Generator&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=patrickjohnson&utm_content=House%20and%20Trees%20Illustration%20Generator&utm_medium=Social_Share
+        ">
+                    Dribble
+                </a>
+            </div>
+            <div className="orginal developer font-semibold">
+                <a href="#">
+                    Careers
+                </a>
+            </div>
+            <div className="orginal developer font-semibold">
+                <a href="#">
+                    Contact
+                </a>
+            </div>
+            <div className="orginal developer font-semibold">
+                <a href="#">
+                    Pricing
+                </a>
+            </div>
+            <div className="orginal developer font-semibold hover:text-orange-300">
+                <a href="https://github.com/Lawi365">
+                    Lawrence
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
